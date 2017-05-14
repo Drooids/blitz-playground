910aeb14e81a6f6b582499a2f0e67ddf94968b6f
Dren Kajmakci <dk18184@gmail.com>
2017-05-14 21:59:19 +0200

    wip: win batch file, project builder

---
+++ scripts/cmake-make.bat
@@ -0,0 +1,29 @@
+@echo OFF
+echo.
+
+if not exist mkdir "bin"
+cd "bin"
+
+WHERE cmake
+if %ERRORLEVEL% NEQ 0 (
+	echo "x - cmake wasn't found"
+	echo "You have to: "
+	echo "1. Google download cmake"
+	echo "2. Install cmake"
+	echo "3. find cmake.exe"
+	echo "4. Set it to system path: _PATH_=%PATH%;C:\path\to\cmake (remove underlines)"
+	exit /b %errorlevel%
+) else (
+	echo "v - cmake found"
+)
+
+:: Build
+
+echo "Bulding stars now..."
+timeout 5
+
+:: cmake --help for diff. versions of VS or platform
+cmake -G "Visual Studio 14 2015 Win64" "C:\Users\navn\Documents\git\blitz-playground"
+
+:: Add to the path
+:: set PATH=%PATH%;C:\some\path
