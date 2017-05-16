e7c27d8e1f940e8d89ac8ab96c68e17c56a38028
Dren Kajmakci <dk18184@gmail.com>
2017-05-16 20:59:16 +0200

    task: command line vs build - closes: #4

--- scripts/cmake-make.bat
+++ scripts/cmake-make.bat
@@ -19,11 +19,22 @@ if %ERRORLEVEL% NEQ 0 (
 
 :: Build
 
-echo "Bulding stars now..."
-timeout 5
-
 :: cmake --help for diff. versions of VS or platform
-cmake -G "Visual Studio 14 2015 Win64" "../"
+cmake -G "Visual Studio 14 2015 Win64" "..\"
+
+:: Path to msbuild
+echo "If you haven't yet, Add the msbuild path to _PATH_(remove underlines) environment."
+reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath
+
+set msbuild.exe=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild.exe=%%D\MSBuild.exe
+
+msbuild.exe "..\bin\ALL_BUILD.vcxproj" /t:Build
+
+echo.
+echo "If your build succeeded, binaries are in:"
+echo "%cd%\bin\Debug"
+echo.
 
 :: Add to the path
 :: set PATH=%PATH%;C:\some\path
