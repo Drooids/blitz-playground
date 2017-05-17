e20d1da060b5986cf428ac011c85c41798042b2c
Dren Kajmakci <dk18184@gmail.com>
2017-05-18 01:13:58 +0200

    wip: texture manager

--- src/example/game/deps/TextureManager.h
+++ src/example/game/deps/TextureManager.h
@@ -1,19 +1,27 @@
 #ifndef TEXTURE_H_DEFINED
 #define TEXTURE_H_DEF
 
-class TextureManager 
+class TextureManager
 {
 public:
-	
+
 	TextureManager();
 	~TextureManager();
 
 	static TextureManager* Instance();
 
-	bool load(std::string fileName, std::string id, 
+	bool load(std::string fileName, std::string id,
 	SDL_Renderer* pRenderer);
 
-	
+	void draw(std:string id, int x, int y, int width, int heght,
+	SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	void drawFrame(std:string id, int x, int y, int width, int heght,
+	int currentRow, SDL_Renderer* pRenderer,
+	SDL_RendererFlip flip = SDL_FLIP_NONE);
+
 private:
+
+	std::map<std::string, SDL_Texture*> m_textureMap;
 
 }
