6b6ddb098f60aa1ac50aedde833455248e8b10a9
dud3 <dk18184@gmail.com>
2017-05-31 10:52:12 +0200

    task: ability to specify the compiler on gnu/linux

--- scripts/cmake-make.sh
+++ scripts/cmake-make.sh
@@ -6,6 +6,14 @@ BUILD_TYPE=${BUILD_TYPE:-Debug}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin
 cd $DIR/../bin
-cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../
+
+if [ -z "$2" ]
+  then
+    cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}  ../
+else
+    echo ">> I'm here..."
+    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_COMPILER=$2 . ../
+fi
+
 make --no-print-directory
 make $*
